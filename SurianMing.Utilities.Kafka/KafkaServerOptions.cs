4525440ddbae79335e47b4a55b1aae12db2ce511
Matt Astill <matt@surianming.co.uk>
2026-03-27 16:57:22 +0000

    Minor updates for kafka logging

--- SurianMing.Utilities.Kafka/KafkaServerOptions.cs
+++ SurianMing.Utilities.Kafka/KafkaServerOptions.cs
@@ -4,6 +4,7 @@ internal class KafkaServerOptions
 {
     public required string BootstrapServers { get; set; }
     public required string SecurityProtocol { get; set; }
+    public int LivenessLogIntervalSeconds { get; set; } = 30;
     public string? SaslMechanism { get; set; }
     public string? SaslUsername { get; set; }
     public string? SaslPassword { get; set; }
